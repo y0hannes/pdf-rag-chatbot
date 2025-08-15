9539f6d7c47a44324c3fef6ebb9944d65416de49
y0hannes <yohanneset3@gmail.com>
2025-08-15 16:38:01 +0300

    fix(imports): resolve deprecation warnings

--- app/ingest.py
+++ app/ingest.py
@@ -1,11 +1,12 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 import os
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
 
 DATA_DIR = "data"
 PERSIST_DIR = "chroma_db"
+
 
 def load_pdfs(data_dir):
     docs = []
@@ -17,6 +18,7 @@ def load_pdfs(data_dir):
             docs.extend(loader.load())
     return docs
 
+
 def chunk_documents(docs, chunk_size=1000, chunk_overlap=200):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
@@ -26,15 +28,18 @@ def chunk_documents(docs, chunk_size=1000, chunk_overlap=200):
     print(f"Split into {len(chunks)} chunks.")
     return chunks
 
+
 def build_chroma_index(chunks, persist_dir):
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2")
     vectordb = Chroma.from_documents(
         documents=chunks,
         embedding=embeddings,
         persist_directory=persist_dir
     )
-    vectordb.persist()
+
     print(f"Saved ChromaDB to: {persist_dir}")
+
 
 if __name__ == "__main__":
     documents = load_pdfs(DATA_DIR)
